ef5a736b00b9f558e8053229078117b20bdd50d0
Kevin <kevtehpro35@gmail.com>
2015-04-20 01:15:34 -0400

    Fixed Http Requests to authenticate username and password

--- com/furryfaust/patterns/multiplayer/Multiplayer.java
+++ com/furryfaust/patterns/multiplayer/Multiplayer.java
@@ -8,27 +8,33 @@ public class Multiplayer {
     public boolean isConnected;
 
     public Multiplayer() {
-        checkConnection();
+        checkConnection("Kevin", "bear");
     }
 
-    public void checkConnection() {
+    public void checkConnection(String username, String password) {
         Net.HttpRequest request = new Net.HttpRequest(Net.HttpMethods.GET);
-        request.setUrl("http://furryfaust.com");
+        request.setUrl("http://localhost:3000/api/auth?username=" + username + "&password="
+                + password);
         Net.HttpResponseListener response = new Net.HttpResponseListener() {
+
             @Override
             public void handleHttpResponse(Net.HttpResponse httpResponse) {
-                isConnected = true;
+                Gdx.app.log("Status Code", String.valueOf(httpResponse.getStatus().getStatusCode()));
+                Gdx.app.log("Response", httpResponse.getResultAsString());
+                isConnected = Boolean.valueOf(httpResponse.getResultAsString());
             }
 
             @Override
             public void failed(Throwable t) {
                 isConnected = false;
+                Gdx.app.log("Status Code", "Failed");
             }
 
             @Override
             public void cancelled() {
                 isConnected = false;
             }
+
         };
         Gdx.net.sendHttpRequest(request, response);
     }
