f6e4429346e214f51d3f94a4643699370be30b5d
Kevin <kevtehpro35@gmail.com>
2015-05-16 12:19:03 -0400

    Finish matches data retrieve

--- com/furryfaust/patterns/multiplayer/screens/GameScreen.java
+++ com/furryfaust/patterns/multiplayer/screens/GameScreen.java
@@ -13,8 +13,13 @@ public class GameScreen implements Screen {
     int index = 1;
     ArrayList<Match> gameData;
 
-    public GameScreen(Core cor) {
-        this.core = cor;
+    public GameScreen(Core core) {
+        this.core = core;
+
+    }
+
+    @Override
+    public void show() {
         core.multiplayer.requestGames(core.multiplayer.usernameStore, core.multiplayer.passwordStore);
         Timer.schedule(new Timer.Task() {
 
@@ -25,8 +30,14 @@ public class GameScreen implements Screen {
                 int min = (index - 1) * 5;
                 int max = index * 5;
                 String idQuery = "";
-                for (; min != max + 1; min ++) {
-                  idQuery += ids[min] + (min != max ? "," : "");
+                for (; min != max + 1; min++) {
+                    if (ids.length - 1 >= min) {
+                        idQuery += ids[min] + (min != max ? "," : "");
+                    }
+                }
+
+                if (idQuery.charAt(idQuery.length() - 1) == ',') {
+                    idQuery = idQuery.substring(0, idQuery.length() - 1);
                 }
 
                 core.multiplayer.infoGames(core.multiplayer.usernameStore, core.multiplayer.passwordStore,
@@ -37,16 +48,14 @@ public class GameScreen implements Screen {
                     @Override
                     public void run() {
                         String result = core.multiplayer.temp;
+                        String[] data = result.split("<br>");
+                        for (int i = 0; i != data.length; i++) {
+                            gameData.add(new Match(data[i]));
+                        }
                     }
                 }, 1);
             }
         }, 1);
-
-    }
-
-    @Override
-    public void show() {
-
     }
 
     @Override
@@ -82,20 +91,30 @@ public class GameScreen implements Screen {
     public class Match {
 
         public HashMap<String, String> data;
+        public String id;
         public String expiry;
 
         public Match(String matchData) {
             data = new HashMap<String, String>();
-            String[] expirySplit = matchData.split("\\|");
-            expiry = expirySplit[1];
-            String[] playerSplit = expirySplit[0].split(" ");
-            String playerA = playerSplit[0].substring(playerSplit[0].indexOf("-") - 1);
-            String playerB = playerSplit[1].substring(playerSplit[1].indexOf("-") - 1);
-            if (!playerSplit[0].contains("{start}") && !playerSplit[0].contains("{end}") && !playerSplit[0].contains("")) {
-                String[] params = playerSplit[0].split("-");
+            String[] separation = matchData.split("\\|");
+            id = separation[0];
+            expiry = separation[2];
+            String unprocessed = separation[1];
+            String[] players = unprocessed.split(" ");
+            for (int i = 0; i != players.length; i++) {
+                String player = players[i];
+                String[] playerData = player.split("-");
+                if (player.contains("{start}") || player.contains("{end}") || player.contains("{moves}")) {
+                    data.put(playerData[0], "INCOMPLETE");
+                } else if (!player.contains("{start}") && !player.contains("{end}") && !player.contains("{moves}")) {
+                    int start = Integer.valueOf(playerData[1]);
+                    int end = Integer.valueOf(playerData[2]);
+                    int moves = Integer.valueOf(playerData[3]);
+
+                    data.put(playerData[0], (end - start) + "|" + moves);
+                }
             }
-
         }
-
     }
+    
 }
